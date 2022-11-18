387a91a89b1d323641c275c6903ef9986be7ed92
Kevin Medri <kevin@netright.co.nz>
2022-11-18 18:17:11 +1300

    Update 2_About.py

--- pages/2_About.py
+++ pages/2_About.py
@@ -1,6 +1,115 @@
 import streamlit as st
-st.image("https://tinyurl.com/2eyvatxh")
-st.markdown('## Project Overview')
+import pandas as pd
+
+st.set_page_config(page_title='Home', layout='wide')
+
+st.markdown(
+    """
+    <style>
+        h1#liverpool {
+        padding: 0;
+        }
+    </style>
+    """, unsafe_allow_html=True
+)
+
+col1, col2 = st.columns((1, 5))
+with col1:
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Omdena-Logo.png?raw=true")
+with col2:
+    st.write('# Liverpool Chapter')
+st.markdown('## Organisation')
+col1, col2 = st.columns((1, 1))
+with col1:
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/pipeline.jpg?raw=true")
+with col2:
+    st.markdown('### Predict RTC Severity')
+    st.markdown('st.markdown')
+    st.markdown('Bonus: create artificial data with improvements in hotspot areas and demonstrate whether or not this reduces the prediction severity ')
+    st.markdown('### Geospatial Heatmap')
+    st.markdown('Create a robust UK heat map to illustrate the collision hotspots.')
+    st.markdown('This maybe done by using a Spatial-Temporal Residual Networks.')
+    st.markdown('You must be able to scroll through the years which adjusts the map')
+    st.markdown('Bonus: create artifical data with improvements in hotspot areas and demonstrate whether or not this reduces the occurrence of accidents')
+    st.markdown('### Time Series Analysis & Predictions')
+    st.markdown('Plot the accident occurence over the UK, forecast the rate with the data as is.')
+    st.markdown('Bonus: Focus on smaller areas of the UK and compare them with artificial improvements')
+    st.markdown('### Vehicle Analysis & Prediction')
+    st.markdown('In-depth analysis of vehicles involved in collisions and create a model to predict the likelihood of specific cohorts being involved in a collision')
+    st.markdown('Bonus: predicting what class of vehicle is in specific accidents')
+st.write('## Pipelines')
+de_dict = {
+    'TASK': 'Data Preprocessing',
+    'Lead Engineers': 'Vivek Srinivasan',
+    'Engineers': ['Jahanzeb Hussain', 'Owais Tahir']
+}
+org = pd.DataFrame(de_dict)
+sp_dict = {
+    'TASK': [
+        'Exploratory Data Analysis', 'Feature Engineering',
+        'Model Development & Eval.'
+        ],
+    'Lead Engineers': [
+            'Ibrahim KAISER', 'Nisar Ahmad', 'Michael Welter'
+        ],
+    'Engineers': [
+            'Kevin Medri', 'Joel Luis', 'Lucas Silva/Djazila Souhila Korti'
+        ]
+}
+org1 = pd.DataFrame(sp_dict)
+gad_dict = {
+    'TASK': [
+        'Exploratory Data Analysis', 'Feature Engineering',
+        'Model Development & Eval.'
+        ],
+    'Lead Engineers': [
+            'Usman Ayaz', 'Francisca Ngeno', 'Monisha Ayelligadala'
+        ],
+    'Engineers': [
+            'Neethu Prabhakaran', 'Dilane FOGUE KAMGA', 'Puneet Surya'
+        ]
+}
+org2 = pd.DataFrame(gad_dict)
+tsa_dict = {
+    'TASK': [
+        'Exploratory Data Analysis', 'Feature Engineering',
+        'Model Development & Eval.'
+        ],
+    'Lead Engineers': [
+            'Cannel Maina', 'Aakanksha', 'Dhananjai Singh'
+        ],
+    'Engineers': [
+            'Mahrukh waqar', 'Wajeeha Imtiaz', 'Syed Muhammad Mubashir Rizvi'
+        ]
+}
+org3 = pd.DataFrame(tsa_dict)
+vap_dict = {
+    'TASK': [
+        'Exploratory Data Analysis', 'Feature Engineering',
+        'Model Development & Eval.'
+        ],
+    'Lead Engineers': [
+            'Shrawan Baral/Faizan', 'Gordana Vujovic', 'Shrawan Baral'
+        ],
+    'Engineers': [
+            'Hasaan Maqsood', 'Raza Ali', 'Suneeta Modekurty'
+        ]
+}
+org4 = pd.DataFrame(vap_dict)
+col1, col2 = st.columns((1, 1))
+with col1:
+    st.write('### PL 1 - Severity Prediction')
+    st.table(org1)
+    st.write('### PL 3 - Time Series Analysis & Prediction')
+    st.table(org3)
+    st.write('### Data Engineers')
+    st.table(org)
+with col2:
+    st.write('### PL 2 - Geospatial Accident Data')
+    st.table(org2)
+    st.write('### PL 4 - Vehicle Analysis & Prediction')
+    st.table(org4)
+st.markdown('### Project Overview')
 st.markdown('Over the last few years improvements to roads in the UK have been implemented across the country in order to create a safer roading system with some great effect.')
 st.markdown('The number of RTCs or road traffic collisions are reported to be in decline.')
 st.markdown('However there still seems to be a rise in severe and fatal collisions.')
