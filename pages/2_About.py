1811cc797659646e98f3f797a87b17da12a9dc39
Kevin Medri <kevin@netright.co.nz>
2022-11-18 19:50:13 +1300

    Update 2_About.py

--- pages/2_About.py
+++ pages/2_About.py
@@ -6,9 +6,18 @@ st.set_page_config(page_title='Home', layout='wide')
 st.markdown(
     """
     <style>
-        h1#liverpool {
+        h1#liverpool-chapter {
         padding: 0;
-        }
+    }
+        h1#liverpool-chapter span.css-10trblm.e16nr0p30 {
+        border-bottom: none;
+        font-variant: inherit;
+    }
+        label.css-cgyhhy.effi0qh3, span.css-10trblm.e16nr0p30 {
+        font-weight: bold;
+        font-variant-caps: small-caps;
+        border-bottom: 3px solid #4abd82;
+    }
     </style>
     """, unsafe_allow_html=True
 )
@@ -18,6 +27,14 @@ with col1:
     st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Omdena-Logo.png?raw=true")
 with col2:
     st.write('# Liverpool Chapter')
+
+st.markdown('# Project Overview')
+st.markdown('Over the last few years improvements to roads in the UK have been implemented across the country in order to create a safer roading system with some great effect.')
+st.markdown('The number of RTCs or road traffic collisions are reported to be in decline.')
+st.markdown('However there still seems to be a rise in severe and fatal collisions.')
+st.markdown('Using datasets from the Department of Transport, we hope to be able to uncover the probability of the severity of a collision.')
+
+
 st.markdown('## Organisation')
 col1, col2 = st.columns((1, 1))
 with col1:
@@ -109,30 +126,26 @@ with col2:
     st.table(org2)
     st.write('### PL 4 - Vehicle Analysis & Prediction')
     st.table(org4)
-st.markdown('### Project Overview')
-st.markdown('Over the last few years improvements to roads in the UK have been implemented across the country in order to create a safer roading system with some great effect.')
-st.markdown('The number of RTCs or road traffic collisions are reported to be in decline.')
-st.markdown('However there still seems to be a rise in severe and fatal collisions.')
-st.markdown('Using datasets from the Department of Transport, we hope to be able to uncover the probability of the severity of a collision.')
+
 st.markdown('Using Data Science we will develop and deploy a machine learning model in an effort to predict RTC severity:')
 st.markdown('- Preprocessing')
 st.markdown('- Exploratory Data Analysis')
 st.markdown('- Feature Engineering')
 st.markdown('- Modeling')
 st.markdown('- Machine Learning')
-st.markdown('The project has been broken down into six pipelines:')
+st.markdown('The project has six sections:')
 st.markdown('1. Data Engineering')
-st.markdown('2. Group 1 Predicting RTC Severity')
-st.markdown('3. Group 2 Geospatial Heatmap')
-st.markdown('4. Group 3 Time Series Analysis')
-st.markdown('5. Group 4 Vehicle Analysis and Predictions')
+st.markdown('2. Pipeline 1 Predicting RTC Severity')
+st.markdown('3. Pipeline 2 Geospatial Heatmap')
+st.markdown('4. pipeline 3 Time Series Analysis')
+st.markdown('5. Pipeline 4 Vehicle Analysis and Predictions')
 st.markdown('6. Solution Deployment')
-st.markdown('**Pipeline 1** prepares the datasets for groups 1 - 4')
-st.markdown('**Pipelines 2 - 5** will run concurrently and have three tasks:')
+st.markdown('**Data Engineering** prepares the datasets for pipelines 1 - 4')
+st.markdown('**Pipelines 1 - 4** will run concurrently and have three tasks:')
 st.markdown('- EDA')
 st.markdown('- Feature Engineering')
 st.markdown('- Model Development and Evaluation')
-st.markdown('**Pipeline 6** will bring together the models and create the solution to be deployed.')
+st.markdown('**Solution Deployment** will bring together the models and create the solution to be deployed.')
 st.markdown('Each Pipeline will produce a Jupyter notebook, based on the findings of each of the team members notebooks, for their task.')
 st.markdown('The task lead will then produce a combined notebook, being passed on to the next task until completion of all three tasks.')
 st.markdown('The notebooks will be published on the Omdena Liverpool GitHub site.')
