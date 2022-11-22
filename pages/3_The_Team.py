0f862a3a1d24f2290fea53294e6eecd212dd38d1
Kevin Medri <kevin@netright.co.nz>
2022-11-22 17:06:26 +1300

    Updates

--- pages/3_The_Team.py
+++ pages/3_The_Team.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title='The Team', layout='wide')
 
 st.markdown(
     """
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         .block-container.css-18e3th9.egzxvld2 {
         padding-top: 0;
@@ -59,6 +60,13 @@ st.markdown(
     """, unsafe_allow_html=True
 )
 
+fa_li = """
+<i class="fa-brands fa-linkedin"></i>
+"""
+fa_em = """
+<i class="fad fa-envelope"></i>
+"""
+
 col1, col2 = st.columns((1, 5))
 with col1:
     st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Omdena-Logo.png?raw=true")
@@ -71,19 +79,95 @@ with col1:
     st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/rich_gregson.jpg?raw=true")
     st.header('Rich Gregson')
     st.subheader('Chapter Lead')
-    st.write('I am an accomplished Data Scientist & Analyst with over 10 years of experience. I am well versed in Data Science and Machine Learning and can use advanced data methodologies to reach useful business insights. I am proficient in the all data science stages, including data preprocessing, statistical method application, predictive modeling, data visualisation and result communication.')
+    st.write('I am an accomplished Data Scientist & Analyst with 10+ years of experience. I am well versed in Data Science and Machine Learning using advanced data methodologies to reach useful business insights. I am proficient in data preprocessing, statistical method application, predictive modeling, data visualisation and result communication.')
+    st.write(fa_li, '[linkedin.com/in/rich-gregson](https://www.linkedin.com/in/rich-gregson/)', unsafe_allow_html=True)
 with col2:
     st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Salman_Khaliq.jpg?raw=true")
     st.header('Salmon Khaliq')
     st.subheader('Chief Engineer and Educator')
     st.write('⚡ My Machine Learning journey 🚗 started in 2008 when I learned to code in MATLAB, and then started implementing the code for the numerical methods course the next year. Thats when I joined Dr. Usmanis Lab, where the research focus was on applying machine learning techniques for predictive modeling of time series 🕝 data.')
+    st.write(fa_li, '[linkedin.com/in/salmankhaliq22](https://www.linkedin.com/in/salmankhaliq22/)', unsafe_allow_html=True)
 with col3:
     st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Owais_Tahir.jpg?raw=true")
     st.header('Owais Tahir')
     st.subheader('Team Member')
     st.write('I am doing masters in data sciences, I have spent the past several years immersing myself in data analytics and machine learning. I have experience in data visualization and developing workflows to compare diverse data sources. I have strong communication skills and experience in explaining technical topics to a general audience.')
+    st.write(fa_li, '[linkedin.com/in/owais-tahir-a049b6233](https://www.linkedin.com/in/owais-tahir-a049b6233/)', unsafe_allow_html=True)
 with col4:
     st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/kevin_medri.jpg?raw=true")
     st.header('Kevin Medri')
     st.subheader('Team Member')
     st.write('I am a data specialist working with data from varied market areas. I have years of domain knowledge and am always committed to the projects at hand. I am able to retrieve great business insights, am well versed in stakeholder presentations and working with and managing teams. I have a sharp eye for detail and have great communication skills.')
+    st.write(fa_li, '[www.linkedin.com/in/kevinmedri](https://www.linkedin.com/in/kevinmedri/)', unsafe_allow_html=True)
+
+with col1:
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Francisca_Ngeno.jpg?raw=true")
+    st.header('Francisca Ngeno')
+    st.subheader('Team Member')
+    st.write('This was my first Omdena Project and I have had a great experience in the Liverpool Chapter. I had an amazing team and above all, I have learnt so much. My skills are better than they were two months ago. I acknowledge the efforts that Rich, Salman and everyone else put in while working on the project. Good luck in your future projects.')
+    st.write(fa_li, '[www.linkedin.com/in/francisca-ngeno](https://www.linkedin.com/in/francisca-ngeno-0a0b49222/)', unsafe_allow_html=True)
+with col2:
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Dounia_Sadiky.jpg?raw=true")
+    st.header('Dounia Sadiky')
+    st.subheader('Team Member')
+    st.write('I am an engineer passionate about mathematics, data science, finance, football...I try to find answers to questions related to my fields of interest using my scientific toolbox. I have joined two Omdena projects so far in order to enhance my data science skills and I had the chance to meet amazing people from whom I learned a lot.')
+    st.write(fa_li, '[www.linkedin.com/in/dounia-sadiky-56a729204](https://www.linkedin.com/in/dounia-sadiky-56a729204/)', unsafe_allow_html=True)
+with col3:
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Saleh_Ahmed_Rony.gif?raw=true")
+    st.header('Saleh Ahmed Rony')
+    st.subheader('Team Member')
+    st.write('I am a data specialist working with data from varied market areas. I have years of domain knowledge and am always committed to the projects at hand. I am able to retrieve great business insights, am well versed in stakeholder presentations and working with and managing teams. I have a sharp eye for detail and have great communication skills.')
+    st.write(fa_li, '[www.linkedin.com/in/saleh-ahmed-rony](https://www.linkedin.com/in/saleh-ahmed-rony-135493156/)', unsafe_allow_html=True)
+with col4:
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Wajeeha_Imtiaz.gif?raw=true")
+    st.header('Wajeeha Imtiazi')
+    st.subheader('Team Member')
+    st.write('Wajeeha Imtiaz- A Computer Science student from NUST.')
+    st.write(' ')
+    st.write('Deeply interested in the Artificial Intelligence and Machine Learning domain.')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(fa_li, '[www.linkedin.com/in/wajeeha-imtiaz](https://www.linkedin.com/in/wajeeha-imtiaz-37a7ab197/)', unsafe_allow_html=True)
+
+with col1:
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Enyinnaya_Benjamin_Uzuegbu.jpg?raw=true")
+    st.header('Enyinnaya Uzuegbu')
+    st.subheader('Team Member')
+    st.write('Its Enyinnaya Benjamin uzuegbu from Omdena Liverpool chapter.')
+    st.write(' ')
+    st.write(' I have one year experience in data science. Having graduated from University of East London.')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(fa_em, '[www.linkedin.com/in/enyinnaya-uzuegbu](https://www.linkedin.com/in/enyinnaya-benjamin-uzuegbu/)', unsafe_allow_html=True)
+with col2:
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/head-placeholder.png?raw=true")
+    st.header('Jarukamol')
+    st.subheader('Team Member')
+    st.write('Jarukamol Dawkrajai is a PhD student working in chemical process fault detection using deep learning.')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(fa_em, '[www.linkedin.com/in/dounia-sadiky-56a729204](https://www.linkedin.com/in/dounia-sadiky-56a729204/)', unsafe_allow_html=True)
+with col3:
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/head-placeholder.png?raw=true")
+    st.header('Lavanya')
+    st.subheader('Team Member')
+    st.write('Lavanya Galgali is a BE graduate with 6 yrs of HR domain experience and exploring data in Machine learning  & AI.')
+    st.write(fa_li, '[www.linkedin.com/in/kevinmedri](https://www.linkedin.com/in/kevinmedri/)', unsafe_allow_html=True)
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+with col4:
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/head-placeholder.png?raw=true")
+    st.header('Kevin Medri')
+    st.subheader('Team Member')
+    st.write('I am a data specialist working with data from varied market areas. I have years of domain knowledge and am always committed to the projects at hand. I am able to retrieve great business insights, am well versed in stakeholder presentations and working with and managing teams. I have a sharp eye for detail and have great communication skills.')
+    st.write(fa_li, '[www.linkedin.com/in/kevinmedri](https://www.linkedin.com/in/kevinmedri/)', unsafe_allow_html=True)
