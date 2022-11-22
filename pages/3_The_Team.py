71fcb61d52dd0a36d822a34512f8eb359d190407
Kevin Medri <kevin@netright.co.nz>
2022-11-22 13:29:40 +1300

    Update 3_The_Team.py

--- pages/3_The_Team.py
+++ pages/3_The_Team.py
@@ -48,17 +48,14 @@ st.markdown(
         width: 2rem;
         height: 2rem;
         }
-        h3#chapter-lead span.css-10trblm.e16nr0p30 {
+        h3#chapter-lead span.css-10trblm.e16nr0p30,
+        h3#chief-engineer-and-educator span.css-10trblm.e16nr0p30,
+        h3#team-member span.css-10trblm.e16nr0p30 {
         font-variant: none;
         border-bottom: 2px solid #82a6c0;
         font-size: 1.5rem;
         }
-        h3#chief-engineer-and-educator span.css-10trblm.e16nr0p30 {
-        font-variant: none;
-        border-bottom: 2px solid #82a6c0;
-        font-size: 1.5rem;
-        }
-    </style>
+        </style>
     """, unsafe_allow_html=True
 )
 
@@ -74,19 +71,19 @@ with col1:
     st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/rich_gregson.jpg?raw=true")
     st.header('Rich Gregson')
     st.subheader('Chapter Lead')
-    st.write('Some Text')
+    st.write('I am an accomplished Data Scientist & Analyst with over 10 years of experience. I am well versed in Data Science and Machine Learning and can use advanced data methodologies to reach useful business insights. I am proficient in the all data science stages, including data preprocessing, statistical method application, predictive modeling, data visualisation and result communication.')
 with col2:
     st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Salman_Khaliq.jpg?raw=true")
     st.header('Salmon Khaliq')
     st.subheader('Chief Engineer and Educator')
-    st.write('Some Text')
+    st.write('⚡ My Machine Learning journey 🚗 started in 2008 when I learned to code in MATLAB, and then started implementing the code for the numerical methods course the next year. Thats when I joined Dr. Usmanis Lab, where the research focus was on applying machine learning techniques for predictive modeling of time series 🕝 data.')
 with col3:
     st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Owais_Tahir.jpg?raw=true")
     st.header('Owais Tahir')
     st.subheader('Team Member')
-    st.write('Some Text')
+    st.write('I am doing masters in data sciences, I have spent the past several years immersing myself in data analytics and machine learning. I have experience in data visualization and developing workflows to compare diverse data sources. I have strong communication skills and experience in explaining technical topics to a general audience.')
 with col4:
     st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/kevin_medri.jpg?raw=true")
     st.header('Kevin Medri')
     st.subheader('Team Member')
-    st.write('Some Text')
+    st.write('I am a data specialist working with data from varied market areas. I have years of domain knowledge and am always committed to the projects at hand. I am able to retrieve great business insights, am well versed in stakeholder presentations and working with and managing teams. I have a sharp eye for detail and have great communication skills.')
