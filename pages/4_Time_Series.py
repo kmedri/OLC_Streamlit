7e15306b45e321b8626ef74c5f411db4b394a12f
Kevin Medri <kevin@netright.co.nz>
2022-12-06 13:41:03 +1300

    Updates and additions

---
+++ pages/4_Time_Series.py
@@ -0,0 +1,101 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import time
+
+st.set_page_config(page_title='Time Series', layout='wide')
+
+
+def load_time_series(url):
+    html_string = open(url)
+    return html_string
+
+
+st.markdown(
+    """
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <style>
+        .block-container.css-18e3th9.egzxvld2 {
+        padding-top: 0;
+        }
+        header.css-vg37xl.e8zbici2 {
+        background: none;
+        }
+        span.css-10trblm.e16nr0p30 {
+        color: #2c39b1;
+        }
+        .css-1dp5vir.e8zbici1 {
+        background-image: linear-gradient(
+        90deg, rgb(130 166 192), rgb(74 189 130)
+        );
+        }
+        .css-tw2vp1.e1tzin5v0 {
+        gap: 10px;
+        }
+        h1#liverpool-chapter {
+        padding: 0;
+        }
+        h1#liverpool-chapter span.css-10trblm.e16nr0p30 {
+        border-bottom: none;
+        font-variant: inherit;
+    }
+        label.css-cgyhhy.effi0qh3, span.css-10trblm.e16nr0p30 {
+        font-weight: bold;
+        font-variant-caps: small-caps;
+        border-bottom: 3px solid #4abd82;
+        }
+        div[data-testid="stSidebarNav"] li div a {
+        margin-left: 1rem;
+        padding: 1rem;
+        width: 300px;
+        border-radius: 0.5rem;
+        }
+        div[data-testid="stSidebarNav"] li div::focus-visible {
+        background-color: rgba(151, 166, 195, 0.15);
+        }
+        svg.e1fb0mya1.css-fblp2m.ex0cdmw0 {
+        width: 2rem;
+        height: 2rem;
+        }
+        h3#chapter-lead span.css-10trblm.e16nr0p30,
+        h3#chief-engineer-and-educator span.css-10trblm.e16nr0p30,
+        h3#team-member span.css-10trblm.e16nr0p30 {
+        font-variant: none;
+        border-bottom: 2px solid #82a6c0;
+        font-size: 1.5rem;
+        }
+        #a-safe-space-to-practice span.css-10trblm.e16nr0p30{
+        text-align: center;
+        font-size: 2rem;
+        color: #82a6c0;
+        border-bottom: none;
+        }
+        .css-1al18u3.e1tzin5v0, iframe {
+        height: 1150px;
+        }
+        </style>
+    """, unsafe_allow_html=True
+)
+
+
+col1, col2 = st.columns((1, 5))
+with col1:
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Omdena-Logo.png?raw=true")
+with col2:
+    st.write('# Liverpool Chapter')
+
+st.markdown('# Time Series')
+
+col1, col2 = st.columns((1, 1))
+with col1:
+    with st.spinner('Wait for it...'):
+        time.sleep(1)
+        components.html(load_time_series('data/map_test.html').read())
+
+with col2:
+    st.title('Time Series Using Plotly')
+    st.write('Using Plotly we created a time series of the severe and fatal accidents across the UK.')
+    st.write('By using the latitude and longitude values we managed to plot each accident.')
+    st.write('It then runs on a timer across each day of the year from 1st Jan 2015 to 31st Dec 2017')
+    st.write('If you hover over the map you can see more details')
+    st.write('The map can be paused, you can use the slider to move across dates.')
+    st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/time_series.png?raw=true")
---
+++ pages/5_Visualizations.py
@@ -0,0 +1,196 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+##############################################################################
+############################### FUNCTIONS ####################################
+##############################################################################
+st.set_page_config(page_title='Visualizations', layout='wide')
+
+
+def accidents_to_years_Bar_subplots(df):
+    df_slight = df[df['Accident_Severity'] == 'Slight'][['Accident_Severity', 'Year']].groupby('Year').count()
+    df_slight.reset_index(inplace=True)
+
+    df_serious = df[df['Accident_Severity']=='Serious'][['Accident_Severity', 'Year']].groupby('Year').count()
+    df_serious.reset_index(inplace=True)
+
+    df_fatal = df[df['Accident_Severity'] == 'Fatal'][['Accident_Severity', 'Year']].groupby('Year').count()
+    df_fatal.reset_index(inplace=True)
+
+    fig = make_subplots(rows=3, cols=1, x_title='Years', y_title='Accident Counts')
+
+    fig.append_trace(go.Bar(
+                x=df_slight['Year'], y=df_slight['Accident_Severity'], name='Slight Accidents'
+        ), row=1, col=1)
+    fig.append_trace(go.Bar(
+                x=df_serious['Year'], y=df_serious['Accident_Severity'], name='Serious Accidents'
+        ), row=2, col=1)
+    fig.append_trace(go.Bar(
+                x=df_fatal['Year'], y=df_fatal['Accident_Severity'], name='Fatal Accidents'
+        ), row=3, col=1)
+
+    fig.update_layout(width=1100, height=1200)
+
+
+# fig.update_xaxes(title_text="<b>Years</b>")
+# fig.update_yaxes(title_text="<b>Accidents Count</b> ", secondary_y=False)
+# fig.update_yaxes(title_text="<b>secondary</b> Y - axis ", secondary_y=True)
+    st.header('Accidents vs Years (Bar Graphs)')
+    st.write(fig)
+##############################################################################
+
+
+def accidents_to_years_Line_subplots(df):
+    df_slight = df[df['Accident_Severity'] == 'Slight'][['Accident_Severity', 'Year']].groupby('Year').count()
+    df_slight.reset_index(inplace=True)
+
+    df_serious = df[df['Accident_Severity'] == 'Serious'][['Accident_Severity', 'Year']].groupby('Year').count()
+    df_serious.reset_index(inplace=True)
+
+    df_fatal = df[df['Accident_Severity'] == 'Fatal'][['Accident_Severity', 'Year']].groupby('Year').count()
+    df_fatal.reset_index(inplace=True)
+
+    fig = make_subplots(rows=3, cols=1, x_title='Years', y_title='Accidents Count')
+
+    fig.add_trace(go.Scatter(
+            x=df_slight['Year'], y=df_slight['Accident_Severity'], name='Slight Accidents'
+            ), row=1, col=1)
+    fig.add_trace(go.Scatter(
+            x=df_serious['Year'], y=df_serious['Accident_Severity'], name='Serious Accidents'
+            ), row=2, col=1)
+    fig.add_trace(go.Scatter(
+            x=df_fatal['Year'], y=df_fatal['Accident_Severity'], name='Fatal Accidents'
+            ), row=3, col=1)
+
+    fig.update_layout(width=1100, height=1000)#, title='Accidents vs Years')
+
+    # fig.update_xaxes(title_text="<b>Years</b>")
+    # fig.update_yaxes(title_text="<b>Accidents Count</b> ", secondary_y=False)
+    # fig.update_yaxes(title_text="<b>secondary</b> Y - axis ", secondary_y=True)
+
+    st.header('Accidents vs Years (Line Graphs)')
+    st.write(fig)
+##############################################################################
+
+
+def accidents_to_years_Line_overlap(df):
+    df_slight = df[df['Accident_Severity']=='Slight'][['Accident_Severity', 'Year']].groupby('Year').count()
+    df_slight.reset_index(inplace=True)
+
+    df_serious = df[df['Accident_Severity']=='Serious'][['Accident_Severity', 'Year']].groupby('Year').count()
+    df_serious.reset_index(inplace=True)
+
+    df_fatal = df[df['Accident_Severity']=='Fatal'][['Accident_Severity', 'Year']].groupby('Year').count()
+    df_fatal.reset_index(inplace=True)
+
+    fig = make_subplots(rows=3, cols=1)
+
+    fig.add_trace(go.Scatter(
+            x=df_slight['Year'], y=df_slight['Accident_Severity'], name='Slight Accidents'
+            ))  # , row=1, col=1)
+    fig.add_trace(go.Scatter(
+            x=df_serious['Year'], y=df_serious['Accident_Severity'], name='Serious Accidents'
+            ))  # , row=2, col=1)
+    fig.add_trace(go.Scatter(
+            x=df_fatal['Year'], y=df_fatal['Accident_Severity'], name='Fatal Accidents'
+            ))  # , row=3, col=1)
+
+    fig.update_layout(width=1100, height=800)#, title='Accidents vs Years')
+
+    fig.update_xaxes(title_text="<b>Years</b>")
+    fig.update_yaxes(title_text="<b>Accidents Count</b> ", secondary_y=False)
+    fig.update_yaxes(title_text="<b>secondary</b> Y - axis ", secondary_y=True)
+
+    st.header('Accidents vs Years (Line Graphs Overlap)')
+    st.write(fig)
+##############################################################################
+
+
+def urban_rural(df):
+    fig = px.histogram(df, x="Accident_Severity", color="Urban_or_Rural_Area")
+    fig.update_layout(width=1100, height=600)
+    st.header('Accidents Count in Urban and Rural Areas')
+    st.write(fig)
+##############################################################################
+
+
+# Load the DATA and cache.
+@st.cache
+def get_data(url):
+    df = pd.read_parquet(url)
+    return df
+
+
+url = 'data/full_accident_data_time_series.parquet'
+df = get_data(url)
+
+
+def main():
+    st.markdown(
+            """
+            <style>
+                .block-container.css-18e3th9.egzxvld2 {
+                padding-top: 0;
+                }
+                header.css-vg37xl.e8zbici2 {
+                background: none;
+                }
+                span.css-10trblm.e16nr0p30 {
+                color: #2c39b1;
+                }
+                .css-1dp5vir.e8zbici1 {
+                background-image: linear-gradient(
+                90deg, rgb(130 166 192), rgb(74 189 130)
+                );
+                }
+                .css-tw2vp1.e1tzin5v0 {
+                gap: 10px;
+                }
+                h1#liverpool-chapter {
+                    padding: 0;
+                }
+                h1#liverpool-chapter span.css-10trblm.e16nr0p30 {
+                border-bottom: none;
+                font-variant: inherit;
+                }
+                label.css-cgyhhy.effi0qh3, span.css-10trblm.e16nr0p30 {
+                font-weight: bold;
+                font-variant-caps: small-caps;
+                border-bottom: 3px solid #4abd82;
+                }
+                div[data-testid="stSidebarNav"] li div a {
+                margin-left: 1rem;
+                padding: 1rem;
+                width: 300px;
+                border-radius: 0.5rem;
+                }
+                div[data-testid="stSidebarNav"] li div::focus-visible {
+                    background-color: rgba(151, 166, 195, 0.15);
+                }
+                svg.e1fb0mya1.css-fblp2m.ex0cdmw0 {
+                width: 2rem;
+                height: 2rem;
+                }
+                </style>
+            """, unsafe_allow_html=True
+    )
+    col1, col2 = st.columns((1, 5))
+    with col1:
+        st.image("https://raw.githubusercontent.com/kmedri/OLC_Streamlit/style/assets/Omdena-Logo.png?raw=true")
+    with col2:
+        st.write('# Liverpool Chapter')
+    accidents_to_years_Bar_subplots(df)
+    accidents_to_years_Line_subplots(df)
+    # accidents_to_years_Line_overlap(df)
+    urban_rural(df)
+
+
+##############################################################################
+###################################Main#######################################
+##############################################################################
+
+if __name__ == "__main__":
+    main()
